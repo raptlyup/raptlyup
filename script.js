fcbe348a35c0942fbd108caa074c21e570ffca28
raptlyup <raptlyup@gmail.com>
2025-01-16 16:36:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Wait until the page fully loads
 window.addEventListener('load', () => {
     // Set a timeout to remove the loading screen after the camera click animation completes
     setTimeout(() => {
-        // Add the "loaded" class to body to trigger the transition and hide the loading screen
-        document.body.classList.add('loaded');
+        // Remove the loading screen
+        document.getElementById('loading-screen').style.display = 'none';
     }, 3000); // Camera animation lasts for 3 seconds
 });
